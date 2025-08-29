56596bf7c2daacb36c15de59933b0de56e23b358
gustavo portela <portelagustavo947@gmail.com>
2025-08-29 15:13:18 -0300

    .

---
+++ cadastrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela De Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div class="form">
+            <input type="text">
+        </div>
+
+    </form>
+</body>
+</html>
